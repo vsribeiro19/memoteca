389132ece4329eed15c1bd930c97d0577cc6f0b4
Victor Ribeiro <v.leitteribeiro@gmail.com>
2023-04-21 20:46:25 -0300

    Utilizando o two-way Data binding para atualizar o card enquanto insere informações nos inputs.

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
   styleUrls: ['./criar-pensamento.component.css']
 })
-export class CriarPensamentoComponent {
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
+  constructor() {
+
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
+  criarPensamento() {
+    alert('Novo pensamento criado')
+  }
+
+  cancelar() {
+    alert('Cancelado')
+  }
 
 }
