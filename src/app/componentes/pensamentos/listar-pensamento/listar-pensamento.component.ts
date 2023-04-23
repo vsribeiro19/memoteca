1c4652d831831e7a04f6378b9151d7e70fbc5de0
Victor Ribeiro <v.leitteribeiro@gmail.com>
2023-04-23 10:10:16 -0300

    Atualizando o projeto e fazendo deploy no Netlify

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
   styleUrls: ['./listar-pensamento.component.css']
 })
-export class ListarPensamentoComponent {
+export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input',
+      autoria: 'Componente filho',
+      modelo: 'modelo3'
+    }
+  ];
+  constructor() {
 
+  }
+
+  ngOnInit(): void {
+
+  }
 }
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
-  pensamento = {
-    conteudo: 'I love Angular',
-    autoria: 'Alura',
-    modelo: 'modelo3'
+  @Input() pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: ''
   }
   constructor() {
 
